e24b675c7d2b427ea43a5cc2e170499fdce17a4a
LasithaNiroshana <lasithaniroshanawm@gmail.com>
2022-09-27 12:23:30 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'route-navigation-gmaps',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
