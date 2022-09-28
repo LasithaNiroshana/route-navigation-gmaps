05e659f12c4344e4e6921aa2bcc3a56fdc1894d5
LasithaNiroshana <lasithaniroshanawm@gmail.com>
2022-09-28 08:54:46 +0530

    first commit

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {MapDirectionsService} from '@angular/google-maps';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError,map} from 'rxjs/operators';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-explore-container',
@@ -7,9 +11,71 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
+  
+  center: google.maps.LatLngLiteral = {lat: 7.8731, lng: 80.7718};
+  zoom = 12;
 
-  constructor() { }
+  readonly directionsResults$: Observable<google.maps.DirectionsResult|undefined>;
+  readonly directionsMatrix$: Observable< google.maps.DistanceMatrixService|undefined>;
+
+  constructor(mapDirectionsService: MapDirectionsService) { 
+    const request: google.maps.DirectionsRequest = {
+      destination: {lat: 7.0840, lng: 80.0098},
+      origin: {lat: 6.9271, lng: 79.8612},
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+    this.directionsResults$ = mapDirectionsService.route(request).pipe(map(response => response.result));
+  }
 
   ngOnInit() {}
+
+  // showTrackingPosition(position) {
+  //   console.log(`tracking postion:  ${position.coords.latitude} - ${position.coords.longitude}`);
+  //   this.currentLat = position.coords.latitude;
+  //   this.currentLong = position.coords.longitude;
+
+  //   let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //   this.map.panTo(location);
+
+  //   if (!this.marker) {
+  //     this.marker = new google.maps.Marker({
+  //       position: location,
+  //       map: this.map,
+  //       title: 'Got you!'
+  //     });
+  //   }
+  //   else {
+  //     this.marker.setPosition(location);
+  //   }
+  // }
+
+  // showPosition(position) {
+  //   this.currentLat = position.coords.latitude;
+  //   this.currentLong = position.coords.longitude;
+
+  //   let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //   this.map.panTo(location);
+
+  //   if (!this.marker) {
+  //     this.marker = new google.maps.Marker({
+  //       position: location,
+  //       map: this.map,
+  //       title: 'Got you!'
+  //     });
+  //   }
+  //   else {
+  //     this.marker.setPosition(location);
+  //   }
+  // }
+
+  // findMe() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.showPosition(position);
+  //     });
+  //   } else {
+  //     alert("Geolocation is not supported by this browser.");
+  //   }
+  // }
 
 }
