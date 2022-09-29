0206bd6a07376e6f8513490d58e4d656efc056aa
LasithaNiroshana <lasithaniroshanawm@gmail.com>
2022-09-29 09:05:53 +0530

    App directions service implemented

--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import {MapDirectionsService} from '@angular/google-maps';
+import {GoogleMapsModule} from '@angular/google-maps';
 
 @Injectable({
   providedIn: 'root'
